5d946b5d7f9accacc370f245e5270c5c4d34f5c5
diabebathily3-cmd <diabebathily3@gmail.com>
2025-09-01 00:40:09 +0200

    Create Boutique.tsx

---
+++ src/pages/Boutique.tsx
@@ -0,0 +1,8 @@
+export default function Boutique() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Bienvenue dans la Boutique 📱</h1>
+      <p>Découvrez les téléphones Realme disponibles.</p>
+    </div>
+  )
+}
