c38ef102def7115c680591b9664f58c1ad76f1e6
diabebathily3-cmd <diabebathily3@gmail.com>
2025-09-01 00:15:20 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
-import Shop from './pages/Shop'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Shop from './Pages/Shop'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
