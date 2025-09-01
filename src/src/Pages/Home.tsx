d4d1fac117cff2ca8ec3fea0c5687c95526471fa
diabebathily3-cmd <diabebathily3@gmail.com>
2025-09-01 19:26:20 +0200

    Create Home.tsx

---
+++ src/src/Pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>Bienvenue sur Bathily Phones</h1>
+      <p>Votre boutique en ligne de téléphones Realme.</p>
+    </div>
+  );
+}
