cb96807daf0f6f7524590a18f94d9cf0272252b9
diabebathily3-cmd <diabebathily3@gmail.com>
2025-09-01 18:01:30 +0200

    Update and rename Home Shop to src/Pages/Home.tsx

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>Welcome to Home</h1>
+      <p>This is the homepage of your Realme shop website.</p>
+    </div>
+  );
+}
