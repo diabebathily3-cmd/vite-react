400fc586e319210cbf3894f9ff8732799c390a4b
diabebathily3-cmd <diabebathily3@gmail.com>
2025-09-01 17:17:47 +0200

    Update Nav.tsx

--- src/src/components/Nav.tsx
+++ src/src/components/Nav.tsx
@@ -1,1 +1,29 @@
-import Nav from './composants/Nav'
+import { NavLink } from "react-router-dom";
+
+export default function Nav() {
+  const linkStyle: React.CSSProperties = {
+    textDecoration: "none",
+    color: "#333",
+    margin: "0 15px",
+    fontWeight: 600,
+  };
+
+  const activeStyle: React.CSSProperties = {
+    color: "tomato",
+    borderBottom: "2px solid tomato",
+  };
+
+  return (
+    <nav style={{ padding: "15px", borderBottom: "1px solid #ccc", textAlign: "center" }}>
+      <NavLink to="/" style={linkStyle}>
+        Accueil
+      </NavLink>
+      <NavLink
+        to="/boutique"
+        style={({ isActive }) => (isActive ? { ...linkStyle, ...activeStyle } : linkStyle)}
+      >
+        Boutique
+      </NavLink>
+    </nav>
+  );
+}
