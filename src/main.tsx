15170fd8802e9b2264e237ce80b0869f499e9039
diabebathily3-cmd <diabebathily3@gmail.com>
2025-09-01 17:30:45 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,2 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Accueil from './Pages/Accueil'
-import Boutique from './Pages/Boutique'
-import Nav from './composants/Nav'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Accueil />} />
-        <Route path="/boutique" element={<Boutique />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-)
+import Home from "./Pages/Home";
+import Shop from "./Pages/Shop";
