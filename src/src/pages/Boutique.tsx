421af82e9deaa5a11618f8701f1ddbb3c2b61153
diabebathily3-cmd <diabebathily3@gmail.com>
2025-09-01 16:59:35 +0200

    Update Boutique.tsx

--- src/src/pages/Boutique.tsx
+++ src/src/pages/Boutique.tsx
@@ -4,17 +4,67 @@ export default function Boutique() {
       <h1>📱 Boutique Realme</h1>
       <p>Découvrez nos modèles Realme au meilleur prix</p>
 
-      <div style={{ display: "flex", justifyContent: "center", gap: "20px", marginTop: "30px" }}>
-        <div style={{ border: "1px solid #ccc", padding: "15px", borderRadius: "8px", width: "200px" }}>
-          <img src="https://fdn2.gsmarena.com/vv/bigpic/realme-12x.jpg" alt="Realme 12x" width="150" />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "20px",
+          marginTop: "30px",
+          flexWrap: "wrap",
+        }}
+      >
+        {/* Realme 12x */}
+        <div
+          style={{
+            border: "1px solid #ccc",
+            padding: "15px",
+            borderRadius: "8px",
+            width: "200px",
+          }}
+        >
+          <img
+            src="https://fdn2.gsmarena.com/vv/bigpic/realme-12x.jpg"
+            alt="Realme 12x"
+            width="180"
+          />
           <h3>Realme 12x</h3>
           <p>Prix : 249 €</p>
         </div>
 
-        <div style={{ border: "1px solid #ccc", padding: "15px", borderRadius: "8px", width: "200px" }}>
-          <img src="https://fdn2.gsmarena.com/vv/bigpic/realme-gt5.jpg" alt="Realme GT5" width="150" />
+        {/* Realme GT5 */}
+        <div
+          style={{
+            border: "1px solid #ccc",
+            padding: "15px",
+            borderRadius: "8px",
+            width: "200px",
+          }}
+        >
+          <img
+            src="https://fdn2.gsmarena.com/vv/bigpic/realme-gt5.jpg"
+            alt="Realme GT5"
+            width="180"
+          />
           <h3>Realme GT5</h3>
           <p>Prix : 399 €</p>
+        </div>
+
+        {/* Realme C67 */}
+        <div
+          style={{
+            border: "1px solid #ccc",
+            padding: "15px",
+            borderRadius: "8px",
+            width: "200px",
+          }}
+        >
+          <img
+            src="https://fdn2.gsmarena.com/vv/bigpic/realme-c67.jpg"
+            alt="Realme C67"
+            width="180"
+          />
+          <h3>Realme C67</h3>
+          <p>Prix : 199 €</p>
         </div>
       </div>
     </div>
