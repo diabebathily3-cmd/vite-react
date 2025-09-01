2698403e4ffffea7de6dd2ce0951fae1457718e1
diabebathily3-cmd <diabebathily3@gmail.com>
2025-09-01 19:01:20 +0200

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur mon site 📱</h1>
+      <p>Utilisez la navigation pour explorer la boutique.</p>
+    </div>
+  );
+}
