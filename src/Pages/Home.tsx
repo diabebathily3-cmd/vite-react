8e7a8f9b559e1f2cc16a11db755c9604f7777d55
diabebathily3-cmd <diabebathily3@gmail.com>
2025-09-01 18:23:11 +0200

    Delete src/Pages/Home.tsx

--- src/Pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "40px" }}>
-      <h1>Welcome to Home</h1>
-      <p>This is the homepage of your Realme shop website.</p>
-    </div>
-  );
-}
